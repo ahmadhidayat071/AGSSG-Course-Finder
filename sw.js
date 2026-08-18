cbb61a38653e64689731f0de03a936c6958c9dee
ahmadhidayat071 <ahmadhidayat071@gmail.com>
2026-08-19 01:20:20 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE = 'agssg-course-finder-v6';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-512.png',
+  './prime-golf-logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key.startsWith('agssg-course-finder-') && key !== CACHE)
+        .map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  if (event.request.mode === 'navigate' || event.request.destination === 'document') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+    )
+  );
+});
